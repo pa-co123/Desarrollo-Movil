1a4d4d7a25e557680b299bdb4807c1fc28f2d03b
pa-co123 <202260263@ucc.mx>
2025-09-04 20:52:14 -0600

    SplashScreen

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,11 @@
-package com.example.listview;
+package com.example.splashscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +14,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    ListView lista;
-    String[][] data={
-            {"Breaking dead","James Cameron","5 temporadas","1"},
-            {"Vampire Diaries","James Cameron","9 temporadas","2"},
-            {"Breaking dead","James Cameron","5 temporadas","3"},
-            {"Breaking dead","James Cameron","5 temporadas","4"},
-            {"Breaking dead","James Cameron","5 temporadas","4"},
-            {"Breaking dead","James Cameron","5 temporadas","4"},
-            {"Breaking dead","James Cameron","5 temporadas","4"},
-            {"Breaking dead","James Cameron","5 temporadas","4"},
-    };
+    Animation spider1, spider2;
+    ImageView imgspider1, imgspider2;
 
-    int[] images={R.drawable.imagen,R.drawable.imagen,R.drawable.imagen,R.drawable.imagen,R.drawable.imagen,R.drawable.imagen,R.drawable.imagen,R.drawable.imagen};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        lista=(ListView) findViewById(R.id.lista);
-        lista.setAdapter(new Adaptador(this,data,images));
+
+        spider1 = AnimationUtils.loadAnimation(this,R.anim.spider1_anim);
+        spider2 = AnimationUtils.loadAnimation(this,R.anim.spider2_anim);
+
+        imgspider1 = findViewById(R.id.p1);
+        imgspider2 = findViewById(R.id.p2);
+
+        imgspider1.startAnimation(spider1);
+        imgspider2.startAnimation(spider2);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                finish();
+            }
+        },3000);
+
     }
 }
